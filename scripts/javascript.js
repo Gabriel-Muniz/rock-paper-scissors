3f8fcb6c62e5891c5b47c3c1ba58cae54703d317
Gabriel Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2023-09-14 20:52:49 -0300

    Add playerChoice to the game

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,6 +1,22 @@
-
 function getComputerChoice(){
     let computerSelection = Math.random() * 3;
     computerSelection = Math.ceil(computerSelection);
     return computerSelection;
+}
+
+function getPlayerChoice(){
+    let playerSelection = prompt("Rock, paper or scissors? :");
+    playerSelection = playerSelection.toLowerCase();
+    checkPlayerChoice(playerSelection);
+    switch(playerSelection){
+        case "rock":
+            return 1;
+        break;
+        case "paper":
+            return 2;
+        break;
+        case "scissors":
+            return 3;
+        break;
+    }
 }
