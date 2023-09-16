6a8a624b0fc8ea1505f45d3ea68beaee0275df08
Gabriel Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2023-09-16 18:49:33 -0300

    Refator getPlayerChoice

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,7 +1,15 @@
 const computerSelection = getComputerChoice();
+const playerSelection = getPlayerChoice();
 
 function getComputerChoice(){
     let computerChoice = Math.random() * 3;
     computerChoice = Math.ceil(computerChoice);
     return computerChoice;
 }
+
+function getPlayerChoice(){
+    let playerChoice = prompt("Rock, paper or scissors? :");
+    playerChoice = playerChoice.toLowerCase();
+    return playerChoice;
+}
+console.log(playerSelection)
