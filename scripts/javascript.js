99939a00c36c59392f2b89d595dfc79b521cfca2
Gabriel Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2023-09-16 19:16:41 -0300

    Refactor checkPlayerChoice

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,5 +1,4 @@
-const computerSelection = getComputerChoice();
-const playerSelection = getPlayerChoice();
+let computerSelection, playerSelection;
 
 function getComputerChoice(){
     let computerChoice = Math.random() * 3;
@@ -12,4 +11,22 @@ function getPlayerChoice(){
     playerChoice = playerChoice.toLowerCase();
     return playerChoice;
 }
-console.log(playerSelection)
+function checkPlayerChoice(playerSelection) {
+    if(playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors"){
+        alert("Select a valid option!");
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function test() {
+    computerSelection = getComputerChoice();
+    do {
+        playerSelection = getPlayerChoice();
+    } while (checkPlayerChoice(playerSelection));
+    console.log(playerSelection);
+}
+for (let i = 0; i < 5; i++) {
+    test()
+}
