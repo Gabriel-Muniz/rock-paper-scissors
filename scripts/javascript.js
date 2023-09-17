4647ee64ed2b35cab37cb39e00cb17cc5948450e
Gabriel Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2023-09-17 16:01:26 -0300

    Refactor game logic
    make sure the game stops when a player doesn't have a chance to comeback

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -2,42 +2,40 @@ let computerSelection, playerSelection, winner;
 const roundsNumber = 5;
 
 game()
-
 function game() {
     let winCounts = loseCounts = tieCounts = 0;
     let roundsLeft = roundsNumber;
-    for(let i = 0; i < roundsNumber; i++){
-        switch(round()){
+    for (let i = 0; i < roundsNumber; i++) {
+
+        //Checks if the computer still have chances of winning. If not gives the win to the player instantly.
+        if ((winCounts > roundsNumber / 2) || (winCounts > loseCounts + roundsLeft) && (loseCounts + roundsLeft < roundsNumber)) {
+            console.log(`You won the game already! With ${winCounts} wins against ${loseCounts} loses and ${tieCounts} ties! \nThere's no way he can revert that! :)`);
+            break;
+        } else if (loseCounts > (roundsNumber / 2) || loseCounts > (winCounts + roundsLeft)) {
+            console.log(`You already lose the game... With ${loseCounts} loses against ${winCounts} wins and ${tieCounts} ties! \nThere's no way you can revert that! :(`);
+            break;
+        }
+
+        switch (round()) {
             case "win":
                 winCounts += 1;
-            break;
+                break;
             case "lose":
                 loseCounts += 1;
-            break;
+                break;
             case "ties":
                 tieCounts += 1;
-            break;
+                break;
         }
 
-        if(roundsLeft !== 1){
-             //Checks if the computer still have chances of winning. If not gives the win to the player instantly.
-            if(winCounts > (roundsNumber/2) || winCounts > (loseCounts + roundsLeft)){
-                console.log(`You won the game already! With ${winCounts} wins against ${loseCounts} loses and ${tieCounts} ties! \nThere's no way he can revert that! :)`);
-                break;
-            }else if(loseCounts>(roundsNumber/2)  || loseCounts > (winCounts + roundsLeft)){
-                console.log(`You already lose the game... With ${loseCounts} loses against ${winCounts} wins and ${tieCounts} ties! \nThere's no way you can revert that! :(`);
-                break;
-            }
-        }
-
-        roundsLeft -= 1;   
+        roundsLeft -= 1;
     }
-    if(roundsLeft == 0){
-        if(winCounts>loseCounts){
+    if (roundsLeft == 0) {
+        if (winCounts > loseCounts) {
             console.log(` \nYou won the game with ${winCounts} wins against ${loseCounts} loses and ${tieCounts} ties!.`);
-        }else if(loseCounts > winCounts) {
+        } else if (loseCounts > winCounts) {
             console.log(` \nYou lose the game with ${loseCounts} loses against ${winCounts} wins and ${tieCounts} ties!.`);
-        }else if(winCounts == loseCounts){
+        } else if (winCounts == loseCounts) {
             console.log(`\nWell... that's embarassing, you guys tied...`)
         }
     }
@@ -54,50 +52,50 @@ function round() {
     winner = getWinner(calcResult(playerSelection, computerSelection));
 
     //Use to log winners of each round.
-    switch(winner){
+    switch (winner) {
         case "win":
             console.log(`You won this round! ${convertChoice(playerSelection)} beats ${convertChoice(computerSelection)}!`);
-        break;
+            break;
         case "lose":
             console.log(`You lose this round! ${convertChoice(playerSelection)} lose to ${convertChoice(computerSelection)}!`);
-        break;
+            break;
         case "ties":
             console.log(`That's a tie! Both  choosed ${convertChoice(playerSelection)}!`);
-        break;
+            break;
     }
     return winner;
 }
 
-function getComputerChoice(){
+function getComputerChoice() {
     let computerChoice = Math.random() * 3; //Multiplied by three and ceil the number so the intervals between 0.01 to 1 will be considered as 1 and so on
     computerChoice = Math.ceil(computerChoice);
-    return computerChoice;
+    return 1;
 }
 
-function getPlayerChoice(){
+function getPlayerChoice() {
     let playerChoice = prompt("Rock, paper or scissors? :");
     playerChoice = playerChoice.toLowerCase();
     return playerChoice;
 }
 function checkPlayerChoice(playerSelection) {
-    if(playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors"){
+    if (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors") {
         alert("Select a valid option!");
         return true;
-    }else{
+    } else {
         return false;
     }
 }
 function convertPlayerChoice(playerSelection) {
-    switch(playerSelection){
+    switch (playerSelection) {
         case "rock":
             return 1;
-        break;
+            break;
         case "paper":
             return 2;
-        break;
+            break;
         case "scissors":
             return 3;
-        break;
+            break;
         default:
             return alert("Something went wrong on the conversion");
     }
@@ -106,26 +104,26 @@ function calcResult(playerSelection, computerSelection) {
     return playerSelection - computerSelection;
 }
 
-function getWinner(result){
-    if(result == -2 || result == 1){
+function getWinner(result) {
+    if (result == -2 || result == 1) {
         return "win";
-    }else if(result == 2 || result == -1){
+    } else if (result == 2 || result == -1) {
         return "lose";
-    }else{
+    } else {
         return "ties";
     }
 }
 
-function convertChoice(respectiveChoice){
-    switch(respectiveChoice){
+function convertChoice(respectiveChoice) {
+    switch (respectiveChoice) {
         case 1:
             return "rock";
-        break;
+            break;
         case 2:
             return "paper";
-        break;
+            break;
         case 3:
             return "scissors";
-        break;
+            break;
     }
 }
