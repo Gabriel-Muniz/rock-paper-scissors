fef6a044c30fbd78be4d864d8134937c6c2d679f
Gabriel Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2023-09-17 13:43:16 -0300

    Refactor round and game function

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,4 +1,60 @@
 let computerSelection, playerSelection, winner;
+const roundsNumber = 5;
+
+game()
+
+function game() {
+    let winCounts = loseCounts = tieCounts = 0;
+    let roundsLeft = roundsNumber;
+    for(let i = 0; i < roundsNumber; i++){
+        switch(round()){
+            case "win":
+                winCounts += 1;
+            break;
+            case "lose":
+                loseCounts += 1;
+            break;
+            case "ties":
+                tieCounts += 1;
+            break;
+        }
+        roundsLeft -= 1;
+        if(winCounts > roundsLeft && winCounts > loseCounts){
+            console.log(` \nYou won the game with ${winCounts} wins against ${loseCounts} loses and ${tieCounts} ties!.`);
+            break;
+        }
+    }
+    if(loseCounts >= tieCounts && loseCounts > winCounts) {
+        console.log(` \nYou lose the game with ${loseCounts} loses against ${winCounts} wins and ${tieCounts} ties!.`);
+    }else if(winCounts == loseCounts && winCounts < tieCounts){
+        console.log(`\nWell... that's embarassing, you guys tied...`)
+    }
+    console.log(winCounts, loseCounts, tieCounts)
+}
+
+function round() {
+    computerSelection = getComputerChoice();
+
+    do {
+        playerSelection = getPlayerChoice();
+    } while (checkPlayerChoice(playerSelection));
+
+    playerSelection = convertPlayerChoice(playerSelection);
+    winner = getWinner(calcResult(playerSelection, computerSelection));
+
+    switch(winner){
+        case "win":
+            console.log(`You won this round! ${convertChoice(playerSelection)} beats ${convertChoice(computerSelection)}!`);
+        break;
+        case "lose":
+            console.log(`You lose this round! ${convertChoice(playerSelection)} lose to ${convertChoice(computerSelection)}!`);
+        break;
+        case "ties":
+            console.log(`That's a tie! Both  choosed ${convertChoice(playerSelection)}!`);
+        break;
+    }
+    return winner;
+}
 
 function getComputerChoice(){
     let computerChoice = Math.random() * 3;
@@ -44,7 +100,7 @@ function getWinner(result){
     }else if(result == 2 || result == -1){
         return "lose";
     }else{
-        return "draw";
+        return "ties";
     }
 }
 
@@ -60,31 +116,4 @@ function convertChoice(respectiveChoice){
             return "scissors";
         break;
     }
-}
-
-function round() {
-    computerSelection = getComputerChoice();
-
-    do {
-        playerSelection = getPlayerChoice();
-    } while (checkPlayerChoice(playerSelection));
-
-    playerSelection = convertPlayerChoice(playerSelection);
-    winner = getWinner(calcResult(playerSelection, computerSelection));
-
-    switch(winner){
-        case "win":
-            return `You won this round! ${convertChoice(playerSelection)} beats ${convertChoice(computerSelection)}!`;
-        break;
-        case "lose":
-            return `You lose this round! ${convertChoice(playerSelection)} lose to ${convertChoice(computerSelection)}!`;
-        break;
-        case "draw":
-            return `That's a draw! Both  choosed ${convertChoice(playerSelection)}!`;
-        break;
-    }   
-}
-
-for(let i = 0; i < 5; i++){
-    console.log(round())
 }
