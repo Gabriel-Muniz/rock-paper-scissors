70daea2e886377d960c08e39a06f0f956db8432d
Gabriel Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2023-10-01 14:11:39 -0300

    refactor score system

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,27 +1,28 @@
 const btnChoice = document.querySelectorAll('.choice-btn');
 const containerBtn = document.querySelector('.container-btn');
 const containerOutput = document.querySelector('.container-output');
+const spanWinner = document.createElement('div');
+spanWinner.classList.add('result-board')
 const rock = `\u{1FAA8}`;
 const paper = `\u{1F4F0}`;
 const scissors = `\u{2702}`;
 const xVersus = `\u{0078}`
 let computerChoice;
-let playerScore = computerScore = 0;
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     computerChoice = Math.ceil(Math.random() * 3)
     return computerChoice;
 }
 
-function calcResult(computerChoice, playerChoice){
-    let result = playerChoice - computerChoice;
-
-}
-
 function playRound(playerChoice) {
     getComputerChoice();
     showRound(playerChoice);
+    showWinner(calcResult(computerChoice, playerChoice));
+    checkGameWinner();
 }
+
 
 function convertChoice(choice) {
     switch (choice) {
@@ -40,6 +41,36 @@ function convertChoice(choice) {
     }
 }
 
+function checkGameWinner() {
+   console.log("YOU: " + playerScore);
+   console.log("CPU: " + computerScore);
+}
+
+function calcResult(computerChoice, playerChoice){
+    let result = playerChoice - computerChoice;
+
+    if (result === 0) {
+        spanWinner.textContent = "THAT'S A DRAW"
+        return;
+    }
+
+    if (result === -2 || result === 1) {
+        playerScore++;
+        spanWinner.textContent = "YOU WIN!"
+        return true;
+    }else if(result === 2 || result === -1){
+        computerScore++;
+        spanWinner.textContent = "YOU LOSE!"
+        return false;
+    }
+
+}
+
+function showWinner(result) {
+    containerOutput.appendChild(spanWinner);
+}
+
+
 function showRound(choice) {
     let spanCpu = convertChoice(computerChoice);
     let spanPlayer = convertChoice(+choice);
@@ -49,6 +80,7 @@ function showRound(choice) {
         containerOutput.removeChild(containerOutput.firstChild);
     }
 
+    divVersus.classList.add('round');
     divVersus.textContent = spanPlayer + xVersus + spanCpu;
     containerOutput.appendChild(divVersus)
 }
