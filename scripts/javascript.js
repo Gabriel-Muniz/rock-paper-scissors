072130fd97bc694996318139ee3bffc39a2b074c
Gabriel Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2024-03-03 17:48:42 -0300

    refac code

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,20 +1,32 @@
-function getComputerChoice() {
-  const choices = ['rock', 'paper', 'scissors'];
+const btnChoices = document.querySelectorAll('.choice-btn');
+const scoreCount = document.querySelectorAll('.score-count');
+const output = document.querySelector('.container-output');
 
+const replayBtn = document.createElement('btn');
+replayBtn.textContent = 'Play again?';
+replayBtn.classList.add('replay-btn');
+
+const choices = ['rock', 'paper', 'scissors'];
+const results = {
+  win: 0,
+  lose: 0,
+  draw: 0,
+};
+
+function getComputerChoice() {
   return choices[Math.floor(Math.random() * 3)]; // return a random index making the computer choose randomly
 }
 
-function getPlayerChoice() {
-  let playerMove;
-  do {
-    playerMove = prompt('Rock, papel or scissors?').toLowerCase();
-  } while (
-    playerMove !== 'rock' &&
-    playerMove !== 'paper' &&
-    playerMove !== 'scissors'
-  );
-  return playerMove;
+function getPlayerChoice(choiceId) {
+  return choices[choiceId];
 }
+
+btnChoices.forEach((btnChoice) => {
+  btnChoice.addEventListener('click', () => {
+    const round = playRound(getComputerChoice(), getPlayerChoice(btnChoice.id));
+    updateScore(round);
+  });
+});
 
 function playRound(compChoice, playerChoice) {
   const pointsCalc = {
@@ -22,42 +34,58 @@ function playRound(compChoice, playerChoice) {
     paper: 2,
     scissors: 3,
   };
+
   const roundResults = pointsCalc[playerChoice] - pointsCalc[compChoice];
 
-  console.log(playerChoice, compChoice);
+  console.log(`${playerChoice} x ${compChoice}`);
 
   if (roundResults === 0) return 'draw';
-  return roundResults === -2 || roundResults === 1 ? 'winner' : 'loser';
+  return roundResults === -2 || roundResults === 1 ? 'win' : 'lose';
 }
 
-function playGame() {
-  const results = {
-    wins: 0,
-    loses: 0,
-    draws: 0,
-  };
-
-  for (let i = 0; i < 5; i += 1) {
-    const round = playRound(getComputerChoice(), getPlayerChoice());
-    switch (round) {
-      case 'winner':
-        results.wins += 1;
-        break;
-
-      case 'loser':
-        results.loses += 1;
-        break;
-
-      case 'draw':
-        results.draws += 1;
-        break;
-
-      default:
-        console.log('!!!ERROR!!!');
-        break;
-    }
+function updateScore(round) {
+  if (round === 'reset') {
+    scoreCount[0].textContent = 0;
+    scoreCount[1].textContent = 0;
   }
-  console.table(results);
+  results[round] += 1;
+  scoreCount[0].textContent = results.win;
+  scoreCount[1].textContent = results.lose;
+
+  checkWinner();
 }
 
-playGame();
+function checkWinner() {
+  if (results.win !== 5 && results.lose !== 5) return;
+  disableButtons();
+
+  const spanWinner = document.createElement('span');
+  output.appendChild(spanWinner);
+
+  output.appendChild(replayBtn);
+
+  if (results.win === 5) {
+    spanWinner.textContent = `CONGRATULATIONS! You won!`;
+    return;
+  }
+  spanWinner.textContent = `%#@*! This game sucks!`;
+}
+
+function disableButtons() {
+  btnChoices.forEach((btn) => {
+    btn.disabled = true;
+  });
+}
+
+replayBtn.addEventListener('click', () => {
+  results.win = 0;
+  results.lose = 0;
+  results.draw = 0;
+
+  btnChoices.forEach((btn) => {
+    btn.disabled = false;
+  });
+
+  output.innerHTML = ``;
+  updateScore('reset');
+});
